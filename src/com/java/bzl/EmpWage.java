2f3b7c243301ff7932c2469a3f5df22b7b491d2a
pratikraut25 <pratikraut2512@gmail.com>
2022-10-15 10:10:48 +0530

    Emplyee Wage

---
+++ src/com/java/bzl/EmpWage.java
@@ -0,0 +1,18 @@
+package com.java.bzl;
+import java.util.*;
+public class EmpWage {
+
+	public static void main(String[] args) {
+
+	System.out.println("Welcome to Employee Wage Computation Program.");
+	
+			Random random = new Random();
+			boolean attendance = random.nextBoolean();
+			
+			if (attendance)
+				System.out.println("Employee is present.");
+			else
+				System.out.println("Employee is absent.");
+		}
+
+}
