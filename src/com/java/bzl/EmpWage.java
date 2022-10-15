8e2f221ba4f77d5d8035e2317861ed730c04417a
pratikraut25 <pratikraut2512@gmail.com>
2022-10-15 11:22:32 +0530

    Added: Daily_Wage

--- src/com/java/bzl/EmpWage.java
+++ src/com/java/bzl/EmpWage.java
@@ -4,15 +4,24 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 
+		float dailyWage;
+		int wagePerHour = 20, fullDayHour = 0;
+		
 		System.out.println("Welcome to Employee Wage Computation Program.");
 
 		Random random = new Random();
 		boolean attendance = random.nextBoolean();
 		
-		if (attendance)
+		if (attendance) {
 			System.out.println("Employee is present.");
+			fullDayHour = 8;
+		}
 		else
-			System.out.println("Employee is absent.");			
+			System.out.println("Employee is absent.");
+
+		dailyWage = wagePerHour * fullDayHour;
+		System.out.println("Daily wage of employee: " + dailyWage);
+				
 		}
 
 }
