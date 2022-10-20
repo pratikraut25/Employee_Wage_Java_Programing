3b6fb7425061f583859979de26e684e0ecdbd6e9
pratikraut25 <pratikraut2512@gmail.com>
2022-10-20 10:26:55 +0530

    Added: class method

--- src/com/java/bzl/EmpWage.java
+++ src/com/java/bzl/EmpWage.java
@@ -2,27 +2,26 @@ package com.java.bzl;
 import java.util.*;
 public class EmpWage {
 
-	public static void main(String[] args) {
-
-		float dailyWage, monthlyWage = 0;
-		int wagePerHour = 20, fullDayHour = 0, workingDaysPerMonth = 20, day = 0;
-
-		System.out.println("Welcome to Employee Wage Computation Program.");
-
-		Random random = new Random();
-		int attendance;
-
-		int isPartTime;
-		int workingHoursPerMonth = 0;
+   private static int PartTime=1;
+private static final int Wage_Per_Hour = 20; 
+private static int fullDayHour =2;
+   private static float dailywage, monthlywage;
+   private static int empPerDay, fullDayHour1, workingHoursPerMonth, day; 
+	private static Random random = new Random();
+	private static void findEmpWage() {
+		workingHoursPerMonth = 0;
+		monthlywage = 0;
+		day = 0;
+		System.out.println("\nWelcome to Employee Wage Computation Program.");
 		while (workingHoursPerMonth <= 100 && day != 20) {
 			day++;
-			attendance = random.nextInt(2);
-			isPartTime = 1 + random.nextInt(2);
+			int attendance = random.nextInt(2);
+			PartTime = 1 + random.nextInt(2);
 
 			switch (attendance) {
 			case 1:
 				System.out.println("Employee is present!");
-				switch (isPartTime) {
+				switch (PartTime) {
 				case 1:
 					System.out.println("Employee is part time.");
 					fullDayHour = 4;
@@ -37,11 +36,15 @@ public class EmpWage {
 
 			}
 			workingHoursPerMonth += fullDayHour;
-			dailyWage = wagePerHour * fullDayHour;
-			monthlyWage += dailyWage;
-			System.out.println("Day " + day + " wage of employee: " + dailyWage + "\n");
+			dailywage = Wage_Per_Hour * fullDayHour;
+			monthlywage += dailywage;
+			System.out.println("Day " + day + " wage of employee: " + dailywage + "\n");
 		}
-		System.out.println("\nMonth wage of employee: " + monthlyWage);
+		System.out.println("\nMonth wage of employee: " + monthlywage);
 		System.out.println("\nProgram runs total: " + day + " times.");
 }
+	public static void main(String[]args){
+		findEmpWage();
+	}
+
 }
